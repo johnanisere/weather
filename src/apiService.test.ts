e72fcece3716565c047bb95b1941e46d8b4f5c77
John Anisere <johnanisere@gmail.com>
2019-04-23 10:45:09 +0100

    implement tests for apiService()

---
+++ src/apiService.test.ts
@@ -0,0 +1,24 @@
+import apiService from "./apiService"
+import constants from "./constants"
+
+const setUp = () => {
+  const time = constants.time
+  const weather = constants.weather
+  const payload = constants.location
+
+  const error = "arguments not passed"
+  const expected = {
+    time,
+    weather
+  }
+
+  return { expected, payload, error }
+}
+
+test("apiService() should return expected weather and time", () => {
+  const { expected, payload } = setUp()
+
+  return apiService(payload).then(data => {
+    expect(data).toEqual(expect.objectContaining(expected))
+  })
+})
