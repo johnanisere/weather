07ce2dd366e4c8048d98d6eb9c8b9b1361371773
John Anisere <johnanisere@gmail.com>
2019-04-23 13:04:00 +0100

    implement getWeather

---
+++ src/getWeather.ts
@@ -0,0 +1,8 @@
+const axios = require("axios")
+
+const URL = "https://api.openweathermap.org/data/2.5/weather?q"
+const APPID = "9b1f4b37978b486eeb8287c5d22354fc"
+
+const getCityTime = city => axios.get(`${URL}=${city}&appid=${APPID}`)
+
+module.exports = getCityTime
