64ed88b4797cf8f48b120c12a8d77e9fbcb4067b
John Anisere <johnanisere@gmail.com>
2019-04-23 04:49:55 +0100

    implement test for index.js

---
+++ src/constants.ts
@@ -0,0 +1,56 @@
+const location = "New York, 10005, Tokyo, São Paulo, Pluto"
+const weather = {
+  coord: {
+    lon: -73.99,
+    lat: 40.73
+  },
+  weather: [
+    {
+      id: 500,
+      main: "Rain",
+      description: "light rain",
+      icon: "10n"
+    },
+    {
+      id: 701,
+      main: "Mist",
+      description: "mist",
+      icon: "50n"
+    }
+  ],
+  base: "stations",
+  main: {
+    temp: 287.33,
+    pressure: 1016,
+    humidity: 87,
+    temp_min: 285.93,
+    temp_max: 288.71
+  },
+  visibility: 16093,
+  wind: {
+    speed: 3.6,
+    deg: 360
+  },
+  rain: {
+    "1h": 0.35
+  },
+  clouds: {
+    all: 90
+  },
+  dt: 1555981225,
+  sys: {
+    type: 1,
+    id: 4026,
+    message: 0.0115,
+    country: "US",
+    sunrise: 1555927616,
+    sunset: 1555976502
+  },
+  id: 5128581,
+  name: "New York",
+  cod: 200
+}
+
+const time = "Mon Apr 22 2019 18:17:07 GMT+0100 (West Africa Standard Time)"
+
+export default { location, weather, time }
--- src/index.test.ts
+++ src/index.test.ts
@@ -1,79 +1,29 @@
-const index=require(./index)
+import index from "./index"
+import constants from "./constants"
 
 const setUp = () => {
-  const payload = "New York, 10005, Tokyo, São Paulo, Pluto"
-  const weather = {
-    coord: {
-      lon: -73.99,
-      lat: 40.73
-    },
-    weather: [
-      {
-        id: 500,
-        main: "Rain",
-        description: "light rain",
-        icon: "10n"
-      },
-      {
-        id: 701,
-        main: "Mist",
-        description: "mist",
-        icon: "50n"
-      }
-    ],
-    base: "stations",
-    main: {
-      temp: 287.33,
-      pressure: 1016,
-      humidity: 87,
-      temp_min: 285.93,
-      temp_max: 288.71
-    },
-    visibility: 16093,
-    wind: {
-      speed: 3.6,
-      deg: 360
-    },
-    rain: {
-      "1h": 0.35
-    },
-    clouds: {
-      all: 90
-    },
-    dt: 1555981225,
-    sys: {
-      type: 1,
-      id: 4026,
-      message: 0.0115,
-      country: "US",
-      sunrise: 1555927616,
-      sunset: 1555976502
-    },
-    id: 5128581,
-    name: "New York",
-    cod: 200
-  }
+  const time = constants.time
+  const weather = constants.weather
+  const payload = constants.location
 
-  const time = "Mon Apr 22 2019 18:17:07 GMT+0100 (West Africa Standard Time)"
-
+  const error = "arguments not passed"
   const expected = {
     time,
     weather
   }
-  const error="arguments not passed"
 
-  return { expected,payload,error }
+  return { expected, payload, error }
 }
 
-test("index() should return expected weather and time",()=>{
-  const {expected,payload}=setUp()
+test("index() should return expected weather and time", () => {
+  const { expected, payload } = setUp()
 
   return index(payload).then(data => {
     expect(data).toEqual(expect.objectContaining(expected))
   })
 })
 
-test("index() should return error message",()=>{
-  const {error}=setUp()
+test("index() should return error message", () => {
+  const { error } = setUp()
   expect(index()).toEqual(error)
 })
